01283e3b3f23411c48e8b5dc948c559d215105e3
mgeri <m.geri1974@gmail.com>
2019-09-20 00:25:28 +0200

    go generator update

--- scripts/oapi-codegen-go.sh
+++ scripts/oapi-codegen-go.sh
@@ -4,3 +4,4 @@ go get github.com/deepmap/oapi-codegen/cmd/oapi-codegen
 oapi-codegen --generate types --o ./pkg/api/types.gen.go --package api ./api/volleyscoreboard.yaml
 oapi-codegen --generate server --o ./pkg/api/server.gen.go --package api ./api/volleyscoreboard.yaml
 oapi-codegen --generate client --o ./pkg/api/client.gen.go --package api ./api/volleyscoreboard.yaml
+
