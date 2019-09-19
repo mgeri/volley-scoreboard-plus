cc88cfd399d6fd4cd527a6d016f6b7bce7900ef6
mgeri <m.geri1974@gmail.com>
2019-09-20 00:31:36 +0200

    update angular web app

--- scripts/oapi-codegen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get github.com/deepmap/oapi-codegen/cmd/oapi-codegen
-oapi-codegen --generate types --o ./pkg/api/types.gen.go --package api ./api/volleyscoreboard.yaml
-oapi-codegen --generate server --o ./pkg/api/server.gen.go --package api ./api/volleyscoreboard.yaml
-oapi-codegen --generate client --o ./pkg/api/client.gen.go --package api ./api/volleyscoreboard.yaml
